d0c608883efdde03dd2f98aa301240705774d0e6
Radu Matei <rmatei1@gmail.com>
2015-09-29 16:41:28 +0300

    added pch file

--- build/Build/Intermediates/mytime.build/Release-iphonesimulator/mytime.build/Objects-normal/i386/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/Storage/Radoo/obfuscator/Obfuscation\ Test/Obfuscation\ Test/AppDelegate.m \
-  /Volumes/Storage/Radoo/obfuscator/Obfuscation\ Test/Obfuscation\ Test/AppDelegate.h
--- build/Build/Intermediates/mytime.build/Release-iphonesimulator/mytime.build/Objects-normal/i386/ViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/Storage/Radoo/obfuscator/Obfuscation\ Test/Obfuscation\ Test/ViewController.m \
-  /Volumes/Storage/Radoo/obfuscator/Obfuscation\ Test/Obfuscation\ Test/ViewController.h
